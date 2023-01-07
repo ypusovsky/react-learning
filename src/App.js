edf1c16bcb34f3710ead6dca021ddf7c4ce28994
Yuri Pusovsky <yuri.pusovsky@sharepoint-template.com>
2023-01-07 18:23:14 +0300

    make project as simple as possible for start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [likes, setLikes] = useState(0);
+  const [text, setText] = useState('Init text');
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{likes}</h2>
+      <h2>{text}</h2>
+      <input text={text} onChange={event => setText(event.target.value)}/>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
   );
 }
